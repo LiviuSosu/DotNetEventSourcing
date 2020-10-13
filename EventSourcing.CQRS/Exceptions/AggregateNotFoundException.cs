7d0acd3d39777397a4d0c1f016599f7782f5dabc
LiviuSosu <sosuliviu@gmail.com>
2020-10-13 21:59:08 +0200

    next: convert the Converter

---
+++ EventSourcing.CQRS/Exceptions/AggregateNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventSourcing.CQRS.Exceptions
+{
+    public class AggregateNotFoundException : Exception
+    {
+        public AggregateNotFoundException(string message) : base(message) { }
+    }
+}
