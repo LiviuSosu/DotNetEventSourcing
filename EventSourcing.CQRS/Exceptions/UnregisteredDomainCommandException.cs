980934c02128712e6b2977690b9a38082dfb4372
LiviuSosu <sosuliviu@gmail.com>
2020-10-29 22:32:38 +0100

    next: leftovers

---
+++ EventSourcing.CQRS/Exceptions/UnregisteredDomainCommandException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventSourcing.CQRS.Exceptions
+{
+    public class UnregisteredDomainCommandException : Exception
+    {
+        public UnregisteredDomainCommandException(string message) : base(message) { }
+    }
+}
