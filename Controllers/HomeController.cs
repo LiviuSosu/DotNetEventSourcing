14a739ec6cff04ebfc3abbbd40e3007165d15856
LiviuSosu <sosuliviu@gmail.com>
2024-08-06 17:05:40 +0200

    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,15 +7,29 @@ namespace EventSourcing.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        List<DiaryItemModel> diaryItemModels;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            diaryItemModels = new List<DiaryItemModel>
+            {
+                new()
+                {
+                    Title = "Title 1",
+                    From = new DateTime(2008,12,30)
+                },
+                 new()
+                {
+                    Title = "Title 2",
+                    From = new DateTime(2018,12,30)
+                }
+            };
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(diaryItemModels);
         }
 
         public IActionResult Privacy()
@@ -30,3 +44,10 @@ namespace EventSourcing.Controllers
         }
     }
 }
+
+/*
+ * Resources used for this project:
+ * 1) https://www.codeproject.com/Articles/555855/Introduction-to-CQRS
+ * 2) https://www.codeproject.com/Articles/1212100/Simple-Event-Sourcing-Demo-in-Csharp
+ * 3) https://www.codeproject.com/Articles/5264244/A-Fast-and-Lightweight-Solution-for-CQRS-and-Event
+ */ 
